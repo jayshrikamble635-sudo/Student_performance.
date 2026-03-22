1693da6c051ad981ae1d32335e0d1742aec2dee1
jayshrikamble635-sudo <jayshrikamble635@gmail.com>
2026-03-22 20:28:19 +0530

    Update student_app.py

--- student_app.py
+++ student_app.py
@@ -1,30 +1,8 @@
 import streamlit as st
-from student_data_csv import save_student, read_students
 
-st.title("Student Management System")
+st.title("Student App Working")
 
-menu = st.sidebar.selectbox("Menu", ["Add Student", "View Students"])
+name = st.text_input("Enter Name")
 
-if menu == "Add Student":
-    st.subheader("Add New Student")
-    
-    name = st.text_input("Enter Name")
-    age = st.text_input("Enter Age")
-
-    if st.button("Add"):
-        if name and age:
-            save_student(name, age)
-            st.success("Student Added Successfully")
-        else:
-            st.warning("Please fill all fields")
-
-elif menu == "View Students":
-    st.subheader("Student List")
-    
-    students = read_students()
-    
-    if students:
-        for s in students:
-            st.write("Name:", s[0], "Age:", s[1])
-    else:
-        st.write("No student data found")
+if st.button("Submit"):
+    st.write("Hello", name)
